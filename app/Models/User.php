10c4dcdc949f6a3a3e041e0b87df60fa629dc044
mohammad <m.deheghanifard@gmail.com>
2024-03-26 10:36:00 +0330

    impl user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,9 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
-        'email',
+        'username',
+        "gender",
+        'mobile',
         'password',
     ];
 
