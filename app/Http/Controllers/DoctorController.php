7614c1a22b6866c261b21b4c1f03b7eca76cb775
mohammad <m.deheghanifard@gmail.com>
2024-03-27 09:37:56 +0330

    Implementation of DoctorController class and crud operation

---
+++ app/Http/Controllers/DoctorController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Doctor;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class DoctorController extends Controller
+{
+    public function create(Request $request) : JsonResponse
+    {
+        $validator = Validator::make(
+            $request->all(),
+            [
+                "name" => "required",
+                "mobile" => "required|max:11|min:11",
+                "address" => "required",
+                "resume" => "required",
+                "category_id" => "required|exists:categories,id"
+            ]
+        );
+
+        if ($validator->fails())
+        {
+            return response() -> json(
+                [
+                    "success" => false,
+                    "message" => $validator -> errors() -> first()
+                ],
+                400
+            );
+        }
+
+        $doctor = Doctor::create($request->all());
+
+        return response() -> json(
+            [
+                "success" => true,
+                "message" => "دکتر جدید با موفقیت اضافه شد",
+                "doctor" => $doctor
+            ]
+        );
+    }
+
+    public function update($id,Request $request) : JsonResponse
+    {
+
+        $validator = Validator::make(
+            $request->all(),
+            [
+                "mobile" => "max:11|min:11",
+                "category_id" => "exists:categories,id"
+
+            ]
+        );
+
+        if ($validator->fails())
+        {
+            return response() -> json(
+                [
+                    "success" => false,
+                    "message" => $validator -> errors() -> first()
+                ],
+                400
+            );
+        }
+
+        Doctor::findOrFail($id) -> update($request ->all());
+
+        return response() -> json(
+            [
+                "success" => true,
+                "message" => "دکتر مورد نظر با موفقیت ویرایش شد"
+            ]
+        );
+    }
+
+    public function delete($id) : JsonResponse
+    {
+        Doctor::findOrFail($id) -> delete();
+
+        return response() -> json([
+            "success" => true,
+            "message" => "دکتر مورد نظر با موفقیت حذف شد"
+        ]);
+    }
+
+    public function fetch(): Collection
+    {
+       return Doctor::all();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\DoctorController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,7 +9,14 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+// Category
 Route::get("categories",[CategoryController::class,"fetch"]);
 Route::post("create-category",[CategoryController::class,"create"]);
 Route::put("update-category/{id}",[CategoryController::class,"update"]);
 Route::delete("delete-category/{id}",[CategoryController::class,"delete"]);
+
+// Doctor
+Route::get("doctors",[DoctorController::class,"fetch"]);
+Route::post("create-doctor",[DoctorController::class,"create"]);
+Route::put("update-doctor/{id}",[DoctorController::class,"update"]);
+Route::delete("delete-doctor/{id}",[DoctorController::class,"delete"]);
